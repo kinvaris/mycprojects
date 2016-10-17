b4e71fe6b2a149470622b6281e8c19559631b4ca
Jonas Libbrecht <jonas.libbrecht@yahoo.com>
2016-10-17 21:05:26 +0200

    hello world

---
+++ hello_world/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* My first hello world */
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    printf("You are %d miles away.\n", distance);
+    printf("Hello world\n");
+    
+    return 0;
+}

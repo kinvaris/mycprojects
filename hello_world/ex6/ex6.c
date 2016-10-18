897c1d0919829a687c8a7034f66c35798268e409
Jonas Libbrecht <Jonas.libbrecht@yahoo.com>
2016-10-18 13:45:48 +0200

    playing with objects/structs

---
+++ hello_world/ex6/ex6.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+    char* name;
+    int age;
+    int height;
+    int weight;
+};
+
+struct Person *Person_create(char* name, int age, int height, int weight)
+{
+    struct Person *who = malloc(sizeof(struct Person));
+    assert(who != NULL);
+
+    who->name = strdup(name);
+    who->age = age;
+    who->height = height;
+    who->weight = weight;
+
+    return who;
+}
+
+void Person_destroy(struct Person *who)
+{
+    assert(who != NULL);
+
+    free(who->name);
+    free(who);
+}
+
+void Person_print(struct Person *who)
+{
+    printf("Name: %s\n", who->name);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("testing\n");
+    struct Person *jonas = Person_create("Jonas Libbrecht", 22, 180, 79);
+
+    printf("Jonas is at memory location %p:\n", jonas);
+    Person_print(jonas);
+    printf("Destroying object jonas...\n");
+    Person_destroy(jonas);
+    printf("Jonas is at memory location %p:\n", jonas);
+    Person_print(jonas);    
+
+    return 0;
+}
